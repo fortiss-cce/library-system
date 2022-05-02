16f494548ab49470f86c550a4bc69a1ab27f48bb
Peter Bludau <bludau@fortiss.org>
2022-05-02 11:23:09 +0200

    typing hints in the storage

--- library/persistence/storage.py
+++ library/persistence/storage.py
@@ -1,4 +1,5 @@
-from typing import TYPE_CHECKING
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional
 if TYPE_CHECKING:
     from library.model.book import Book
     from library.model.user import User
@@ -6,150 +7,150 @@ if TYPE_CHECKING:
     from library.model.publisher import Publisher
     from library.payment.invoice import Invoice
 
-users: list["User"] = []
-books: list["Book"] = []
-authors: list["Author"] = []
-publishers: list["Publisher"] = []
-invoices: list["Invoice"] = []
+users: list[User] = []
+books: list[Book] = []
+authors: list[Author] = []
+publishers: list[Publisher] = []
+invoices: list[Invoice] = []
 
 
 class LibraryRepository:
     @staticmethod
-    def read_users():
+    def read_users() -> list[User]:
         return users
 
     @staticmethod
-    def read_books():
+    def read_books() -> list[Book]:
         return books
 
     @staticmethod
-    def read_authors():
+    def read_authors() -> list[Author]:
         return authors
 
     @staticmethod
-    def read_publishers():
+    def read_publishers() -> list[Publisher]:
         return publishers
 
     @staticmethod
-    def read_invoices():
+    def read_invoices() -> list[Invoice]:
         return invoices
 
     # books
     @staticmethod
-    def create_book(book: "Book"):
+    def create_book(book: Book):
         books.append(book)
 
     @staticmethod
-    def read_book(isbn: str):
+    def read_book(isbn: str) -> Optional[Book]:
         found_books = [x for x in books if x.isbn == isbn]
         if len(found_books) > 0:
             return found_books[0]
         return None
 
     @staticmethod
-    def update_book(book: "Book"):
+    def update_book(book: Book):
         index = books.index(book)
         if index >= 0:
             books[index] = book
 
     @staticmethod
-    def delete_book(book: "Book"):
+    def delete_book(book: Book):
         index = books.index(book)
         if index >= 0:
             books.remove(book)
 
     # users
     @staticmethod
-    def create_user(user: "User"):
+    def create_user(user: User):
         users.append(user)
 
     @staticmethod
-    def read_user(email: str):
+    def read_user(email: str) -> Optional[User]:
         found_users = [x for x in users if x.email == email]
         if len(found_users) > 0:
             return found_users[0]
         return None
 
     @staticmethod
-    def update_user(user: "User"):
+    def update_user(user: User):
         index = users.index(user)
         if index >= 0:
             users[index] = user
 
     @staticmethod
-    def delete_user(user: "User"):
+    def delete_user(user: User):
         index = users.index(user)
         if index >= 0:
             users.remove(user)
 
     # author
     @staticmethod
-    def create_author(author: "Author"):
+    def create_author(author: Author):
         authors.append(author)
 
     @staticmethod
-    def read_author(firstname: str, lastname: str):
+    def read_author(firstname: str, lastname: str) -> Optional[Author]:
         found_authors = [x for x in authors if x.firstname == firstname and x.lastname == lastname]
         if len(found_authors) > 0:
             return found_authors[0]
         return None
 
     @staticmethod
-    def update_author(author: "Author"):
+    def update_author(author: Author):
         index = authors.index(author)
         if index >= 0:
             authors[index] = author
 
     @staticmethod
-    def delete_author(author: "Author"):
+    def delete_author(author: Author):
         index = authors.index(author)
         if index >= 0:
             authors.remove(author)
 
     # publisher
     @staticmethod
-    def create_publisher(publisher: "Publisher"):
+    def create_publisher(publisher: Publisher):
         publishers.append(publisher)
 
     @staticmethod
-    def read_publisher(name: str):
+    def read_publisher(name: str) -> Optional[Publisher]:
         found_publishers = [x for x in publishers if x.name == name]
         if len(found_publishers) > 0:
             return found_publishers[0]
         return None
 
     @staticmethod
-    def update_publisher(publisher: "Publisher"):
+    def update_publisher(publisher: Publisher):
         index = publishers.index(publisher)
         if index >= 0:
             publishers[index] = publisher
 
     @staticmethod
-    def delete_publisher(publisher: "Publisher"):
+    def delete_publisher(publisher: Publisher):
         index = publishers.index(publisher)
         if index >= 0:
             publishers.remove(publisher)
 
     # invoice
     @staticmethod
-    def create_invoice(invoice: "Invoice"):
+    def create_invoice(invoice: Invoice):
         invoices.append(invoice)
 
     @staticmethod
-    def read_invoice(id: str):
+    def read_invoice(id: str) -> Optional[Invoice]:
         found_invoices = [x for x in invoices if x.id == id]
         if len(found_invoices) > 0:
             return found_invoices[0]
         return None
 
     @staticmethod
-    def update_invoice(invoice: "Invoice"):
+    def update_invoice(invoice: Invoice):
         index = invoices.index(invoice)
         if index >= 0:
             invoices[index] = invoice
 
     @staticmethod
-    def delete_invoice(invoice: "Invoice"):
+    def delete_invoice(invoice: Invoice):
         index = invoices.index(invoice)
         if index >= 0:
             invoices.remove(invoice)
