4336bc7c1501ec407ee050eb1153b048f6109f52
tianlunhu <htlshr@163.com>
2023-11-23 13:34:03 +0100

    Group 2

--- library/model/book.py
+++ library/model/book.py
@@ -7,23 +7,11 @@ import xml.etree.ElementTree as et
 
 from library.persistence.storage import LibraryRepository
 
-
 class Book:
-    title: str
-    authors: list[Author]
-    publisher: Publisher
-    publication_date: datetime
-    genres: list[Genre]
-    pages: int
-    isbn: str
-
-    existing_items: int
-    borrowed_items: int
-
-    _book_type: str
-    duration: int = 0
-
-    def __init__(self, title, authors, publisher, pub_date, genres, pages, isbn, type, duration=0, existing_items=1, borrowed_items=0):
+    due_date: datetime
+    weekly_fee: float
+    def __init__(self, title, authors, publisher, pub_date, genres, pages, isbn, type, duration: int = 0,
+                 existing_items: int = 1, borrowed_items: int = 0):
         self.title = title
         self.authors = authors
         self.publisher = publisher
@@ -35,24 +23,9 @@ class Book:
         self.duration = duration
         self.existing_items = existing_items
         self.borrowed_items = borrowed_items
+        # self.isBorrowable = self.can_borrow()
 
     @classmethod
-    def from_borrowed_book(cls, borrowed_book: "BorrowedBook") -> "Book":
-        book = Book(
-            borrowed_book.title,
-            borrowed_book.authors,
-            borrowed_book.publisher,
-            borrowed_book.publication_date,
-            borrowed_book.genres,
-            borrowed_book.pages,
-            borrowed_book.isbn,
-            borrowed_book._book_type,
-            borrowed_book.duration,
-            borrowed_book.existing_items,
-            borrowed_book.borrowed_items,
-        )
-        return book
-
     def can_borrow(self) -> bool:
         if self._book_type == "Paper":
             return self.existing_items - self.borrowed_items > 0
@@ -83,88 +56,57 @@ class Book:
         else:
             raise AttributeError("No such book type...")
 
-    def borrow_book(self) -> "BorrowedBook":
+    def borrow_book(self):
         if self.can_borrow():
             if self._book_type == "Paper":
                 self.borrowed_items += 1
             LibraryRepository.update_book(self)
-            borrowed_book = BorrowedBook.from_book(self)
-            borrowed_book.due_date = datetime.now() + timedelta(days=7)
-            borrowed_book.current_fee = self.get_weekly_fee()
-            return borrowed_book
+            self.due_date = datetime.now() + timedelta(days=7)
+            self.current_fee = self.get_weekly_fee()
+            return self
         raise ValueError("Book cannot be borrowed")
 
-    def __eq__(self, other):
-        """Overrides the default implementation"""
-        if isinstance(other, Book) or isinstance(other, BorrowedBook):
-            return self.isbn == other.isbn and self._book_type == other._book_type
-        return NotImplemented
-
-    def __str__(self):
-        return BookSerializer().serialize(self, "JSON")
-
-
-class BorrowedBook(Book):
-    due_date: datetime
-    current_fee: float
-
-    @classmethod
-    def from_book(cls, book: Book) -> "BorrowedBook":
-        borrowed_book = BorrowedBook(
-            book.title,
-            book.authors,
-            book.publisher,
-            book.publication_date,
-            book.genres,
-            book.pages,
-            book.isbn,
-            book._book_type,
-            book.duration,
-            book.existing_items,
-            book.borrowed_items,
-        )
-        return borrowed_book
-
-    def renew_rental(self) -> "BorrowedBook":
+    def renew_rental(self):
         self.due_date += timedelta(days=7)
         self.current_fee += self.get_weekly_fee()
         return self
 
-    def return_book(self) -> Book:
+    def return_book(self):
         if self._book_type == "Paper":
             self.borrowed_items -= 1
-        book = Book.from_borrowed_book(self)
-        LibraryRepository.update_book(book)
-        return book
+        LibraryRepository.update_book(self)
+        return self
 
-    def __eq__(self, other):
-        """Overrides the default implementation"""
-        if isinstance(other, Book) or isinstance(other, BorrowedBook):
-            return self.isbn == other.isbn and self._book_type == other._book_type
-        return NotImplemented
-
-
-class BookSerializer:
-    def serialize(self, book: Book, format: str):
+    def serialize(self, format: str):
         if format == "JSON":
             book_info = {
-                "id": book.isbn,
-                "title": book.title,
-                "authors": [author.get_fullname() for author in book.authors],
-                "available_items": book.existing_items - book.borrowed_items,
-                "borrowed_items": book.borrowed_items,
+                "id": self.isbn,
+                "title": self.title,
+                "authors": [author.get_fullname() for author in self.authors],
+                "available_items": self.existing_items - self.borrowed_items,
+                "borrowed_items": self.borrowed_items,
             }
             return json.dumps(book_info)
         elif format == "XML":
-            book_info = et.Element("book", attrib={"id": book.isbn})
+            book_info = et.Element("book", attrib={"id": self.isbn})
             title = et.SubElement(book_info, "title")
-            title.text = book.title
+            title.text = self.title
             authors = et.SubElement(book_info, "authors")
-            authors.text = ", ".join([author.get_fullname() for author in book.authors])
+            authors.text = ", ".join([author.get_fullname() for author in self.authors])
             avail = et.SubElement(book_info, "available")
-            avail.text = str(book.existing_items - book.borrowed_items)
+            avail.text = str(self.existing_items - self.borrowed_items)
             authors = et.SubElement(book_info, "borrowed")
-            authors.text = str(book.borrowed_items)
+            authors.text = str(self.borrowed_items)
             return et.tostring(book_info, encoding="Unicode")
         else:
             raise ValueError(format)
+
+
+    def __eq__(self, other):
+        """Overrides the default implementation"""
+        if isinstance(other, Book):
+            return self.isbn == other.isbn and self._book_type == other._book_type
+        return NotImplemented
+
+    def __str__(self):
+        return self.serialize("JSON")
--- library/payment/paypal.py
+++ library/payment/paypal.py
@@ -1,2 +1,7 @@
 PAYPAL_DATA_BASE: dict[str, str] = {"amanda1985": "PaSsW0rD", "qwerty": "123456789"}
 PAYPAL_ACCOUNT_BALANCE: dict[str, float] = {"amanda1985": 100.0, "qwerty": 42.0}
+
+class Paypal:
+    def __init__(self, email: str, password: str):
+        self.email = email
+        self.password = password
